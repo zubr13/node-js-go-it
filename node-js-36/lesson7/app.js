4a2a366674e31afdcd80ca963e2e2e88f342f6fa
Andrii Zubrytskyi <andreizubritskiy@gmail.com>
2021-01-19 21:47:51 +0200

    Added lesson 7

---
+++ node-js-36/lesson7/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRouter = require('./user/user.router');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+// 1. Start server
+// 2. Init middlewares
+// 3. Declare routes
+// 4. Connect to database
+// 5. Listen on port
+
+start();
+
+async function start() {
+    const app = initServer();
+    initMiddlewares(app);
+    declareRoutes(app);
+    await connectToDb();
+    listen(app);
+}
+
+function initServer() {
+    return express();
+}
+
+function initMiddlewares(app) {
+    app.use(express.json());
+}
+
+function declareRoutes(app) {
+    app.use('/users', userRouter);
+}
+
+async function connectToDb() {
+    await mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+}
+
+function listen(app) {
+    app.listen(PORT, () => {
+        console.log('Server is listening on port: ', PORT);
+    });
+}
